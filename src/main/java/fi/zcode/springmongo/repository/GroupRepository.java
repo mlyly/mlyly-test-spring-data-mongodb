050391635959d8924806d2ddd3884327d4697826
mlyly <mlyly@10.99.99.42>
2012-12-03 16:17:28 +0200

    Testing paging and @Query + more complex query

--- src/main/java/fi/zcode/springmongo/repository/GroupRepository.java
+++ src/main/java/fi/zcode/springmongo/repository/GroupRepository.java
@@ -12,13 +12,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  */
 public interface GroupRepository extends PagingAndSortingRepository<Group, String> {
 
-//    // @Query(value="{ 'firstname' : ?0 }", fields="{ 'firstname' : 1, 'lastname' : 1}")
-//    // @Query(value="{ 'oid' : ?0 }")
-//    public KoulutusModuli findByOid(String oid);
-//    public Page<KoulutusModuli> findByUlkoinenTunniste(String tunniste, Pageable pageable);
-
+    // TODO This does not work like this....
     @Query(value="{ 'name[fi]' : ?0 }")
     public Collection<Group> findByNameFI(String name);
-
 
 }
--- src/main/java/fi/zcode/springmongo/repository/UserRepository.java
+++ src/main/java/fi/zcode/springmongo/repository/UserRepository.java
@@ -13,22 +13,15 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  */
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
-//    // @Query(value="{ 'firstname' : ?0 }", fields="{ 'firstname' : 1, 'lastname' : 1}")
-//    // @Query(value="{ 'oid' : ?0 }")
-//    public KoulutusModuli findByOid(String oid);
-//    public Page<KoulutusModuli> findByUlkoinenTunniste(String tunniste, Pageable pageable);
-
-
-    @Query(value="{ 'age' : { $lte : ?0 } }", fields="{ 'lastname' : 1, 'age' : 1}")
+    @Query(value = "{ 'age' : { $lte : ?0 } }", fields = "{ 'lastname' : 1, 'age' : 1}")
     public Page<User> findByAgeLTLight(int age, Pageable pageable);
-    
+
     public Collection<User> findByFirstname(String name);
 
-    @Query(value="{ 'firstname' : ?0 }", fields="{ 'lastname' : 1, 'age' : 1}")
+    @Query(value = "{ 'firstname' : ?0 }", fields = "{ 'lastname' : 1, 'age' : 1}")
     public Collection<User> findByFirstnameLight(String firstname);
 
     public Collection<User> findByLastname(String name);
 
     public User findByUsername(String username);
-
 }
